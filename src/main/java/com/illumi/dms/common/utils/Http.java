15ffc6393a9f4fd5c1df508b602365f1c327e6cd
pengchuhuai <pengch@pokermaster.com>
2018-04-27 19:22:58 +0800

    utils

--- src/main/java/com/illumi/dms/common/utils/Http.java
+++ src/main/java/com/illumi/dms/common/utils/Http.java
@@ -1,22 +1,11 @@
 package com.illumi.dms.common.utils;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import javax.net.ssl.*;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 public class Http
 {
